8ed64b54b6c67bf96c3fe4c1918d583814c6d2cc
Andrea Pasquale <andrea.pasquale@polimi.it>
2023-09-10 15:35:41 +0200

    minor

--- src/SolarPressure/cannonball.jl
+++ src/SolarPressure/cannonball.jl
@@ -7,20 +7,17 @@ The cannonball model is the simplest way to approximate the SRP acceleration. Th
 considers the SRP acceleration to be constant along the Sun-spacecraft direction,
 where the acceleration magnitude depends on the area-to-mass ratio and a 
 reflectivity coefficient `Cr = 1+ρ`. `Asc` is the spacecraft equivalent are, `Msc` its 
-mass and `s` the Sun-to-spacecraft position vector.
+mass, `P` the solar pressure and `s` the Sun-to-spacecraft position vector.
 
 ### References
 
 - Zardaın, L., Farrés, A., & Puig, A. (2020). High-fidelity modeling and visualizing of 
   solar radiation pressure: a framework for high-fidelity analysis. UMBC Faculty Collection.
 """
-@fastmath function compute_srp_cannonball(ρ::T, Asc::T, Msc::T, s::AbstractVector{T}) where T 
+@fastmath function compute_srp_cannonball(ρ::T, Asc::T, Msc::T, P::T, s::AbstractVector{T}) where T 
     sv = SA[s[1], s[2], s[3]]
     svn = sqrt(sv[1]*sv[1] + sv[2]*sv[2] + sv[3]*sv[3])
     svn3 = svn*svn*svn
-
-    # Compute solar pressure
-    P = compute_solar_pressure(svn) 
     
     return - (1+ρ)*P*Asc/Msc * sv/svn3
 end
--- src/SolarPressure/flatplate.jl
+++ src/SolarPressure/flatplate.jl
@@ -1,6 +1,6 @@
 """
-    compute_srp_flatplate(ρs::T, ρd::T, Asc::T, Msc::T, 
-        s::AbstractVector{T}, n::AbstractVector{T}) where T 
+    compute_srp_flatplate(ρsi::T, ρdi::T, Ai::T, Msc::T, P::T, uni::AbstractVector{T}, 
+        us::AbstractVector{T}) where T 
 
 Compute SRP acceleration with the plat-plate model.
 
@@ -10,30 +10,33 @@ of the acceleration. In this model the spacecraft is approximated by a collectio
 each of them having different reflectivity properties, allowing the SRP magnitude to vary 
 depending on the spacecraft's orientation with respect to the Sun-spacecraft line.
 
+Notice that a flat plate only reflects on one side, hence if the light hits the back side of a plate its
+SRP should not be counted. The only disadvantage of this approximation is that it does not 
+take into account possible auto occultation between the different plates.
+
+Here `ρsi` and `ρdi` are respectively the rates specular and diffusive reflection, `Ai` the 
+plate surface, `Msc` the spacecraft mass, `P` the solar pressure at the current distance from 
+the Sun, `us` the Sun-to-spacecraft unit vector and `uni` the plate normal.
+
 ### References
 
 - Zardain, L., Farrés, A., & Puig, A. (2020). High-fidelity modeling and visualizing of 
   solar radiation pressure: a framework for high-fidelity analysis. UMBC Faculty Collection.
 """
-@fastmath function compute_srp_flatplate(ρs::T, ρd::T, Asc::T, Msc::T, 
-    s::AbstractVector{T}, n::AbstractVector{T}) where T 
+@fastmath function compute_srp_flatplate(ρsi::T, ρdi::T, Ai::T, Msc::T, P::T,
+    uni::AbstractVector{T}, us::AbstractVector{T}) where T 
 
     # Compute unit vectors and illumination
-    us = unitvec(s)
-    un = unitvec(n)
-    cosθ = us[1]*un[1] + us[2]*un[2] + us[3]*un[3]
+    cosθ = us[1]*uni[1] + us[2]*uni[2] + us[3]*uni[3]
 
     if cosθ < 0 
-        # Compute solar pressure
-        P = compute_solar_pressure(svn) 
-        
         # Compute acceleration components along sun and normal direction
-        as = (1-ρs) * us 
-        an = 2*(ρs*cosθ + ρd/3) * un
+        as = (1-ρsi) * us 
+        an = 2*(ρsi*cosθ + ρdi/3) * uni
 
         # Compute total acceleration
         # Zardain, eq. 5
-        return P * Asc/Msc * cosθ * ( as + an )
+        return P * Ai/Msc * cosθ * ( as + an )
     else 
         return T(0.)
     end
