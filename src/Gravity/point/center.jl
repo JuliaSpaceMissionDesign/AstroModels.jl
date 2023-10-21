ed5af497f35dd3aa18228b65428ccf7ea46df0bf
Andrea Pasquale <andrea.pasquale@polimi.it>
2023-10-21 18:36:03 +0200

    fixed typo

--- src/Gravity/point/center.jl
+++ src/Gravity/point/center.jl
@@ -24,7 +24,7 @@ Compute acceleration due to the central body. Here `r` is the vector from the ce
 to the particle.
 """
 @fastmath function compute_twobody(μ::T, pos::AbstractVector{T}) where T 
-    r = SA[pos[1], pos[2], pos[2]]
+    r = SA[pos[1], pos[2], pos[3]]
     rn2 = r[1]*r[1] + r[2]*r[2] + r[3]*r[3]
     rn = sqrt(rn2)
     rn3 = rn2*rn 
