daefa31ee0bb9e15fb878053353b6bab8a7f1dbb
Andrea Pasquale <andrea.pasquale@polimi.it>
2024-04-06 14:54:50 +0200

    minor on tests timing

--- test/Gravity/Gravity.jl
+++ test/Gravity/Gravity.jl
@@ -7,10 +7,10 @@ using Test
 using AstroModels 
 using AstroModels.Gravity
 
-@testset "Spherical Harmonics" verbose=true begin
+@time @testset "Spherical Harmonics" verbose=true begin
     include("harmonics.jl")
 end;
 
-@testset "Polyhedron" verbose=true begin 
+@time @testset "Polyhedron" verbose=true begin 
     include("polyhedron.jl")
 end;
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,7 @@
 using Test
 
 @time begin 
-    @time @testset "Gravity" verbose=true begin
+    @testset "Gravity" verbose=true begin
         include("Gravity/Gravity.jl") 
     end
 end;
