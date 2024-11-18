433a959bac106a013df589c8ca256a73014b2e48
Andrea Pasquale <andrea.pasquale@polimi.it>
2024-11-18 21:38:10 +0100

    update atmosphere interface

--- src/Atmosphere/interface.jl
+++ src/Atmosphere/interface.jl
@@ -6,6 +6,15 @@ Abstract type for all density models.
 abstract type AbstractDensityModel{T} <: AbstractJSMDModel end
 
 """
-    compute()
+    compute_density(model, pos)
+
+Compute density based on a given model in a given position.
 """
 function compute_density end
+
+"""
+    compute_wind(model, pos)
+
+Compute wind based on a given model in a given position.
+"""
+function compute_wind end
